5e3addf5a55b5e3b48ff0e7b860f2792d6b79c0e
Сергій Гололобов <gololobov.serhiy@gmail.com>
2023-05-09 20:51:17 +0300

    Добавьте файлы проекта.

---
+++ PV221_Forms/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace PV221_Forms
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new System.Windows.Forms.Button();
+            label1 = new System.Windows.Forms.Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new System.Drawing.Point(584, 195);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(128, 63);
+            button1.TabIndex = 1;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click_1;
+            // 
+            // label1
+            // 
+            label1.Location = new System.Drawing.Point(43, 39);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(514, 219);
+            label1.TabIndex = 2;
+            label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            BackColor = System.Drawing.SystemColors.Control;
+            ClientSize = new System.Drawing.Size(774, 404);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            Name = "Form1";
+            StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            Text = "Form1";
+            MouseClick += Form1_MouseClick;
+            MouseMove += Form1_MouseMove;
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ PV221_Forms/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PV221_Forms
+{
+    public partial class Form1 : Form
+    {
+        List<Label> labels = new List<Label>();
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult r = MessageBox.Show("Hello WinForm", "New Form", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (r == DialogResult.Yes)
+            {
+                MessageBox.Show("Yes");
+            }
+            else
+            {
+                MessageBox.Show("No");
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.Text = "X = " + e.X.ToString() + ", Y = " + e.Y.ToString();
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                MessageBox.Show("Left");
+            }
+
+            if (e.Button == MouseButtons.Right)
+            {
+                MessageBox.Show("Right");
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Label l = new Label();
+            labels.Add(l);
+            
+
+        }
+
+
+    }
+}
